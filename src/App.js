a537013db2fe275b13af11a0a001a9c488726f94
nishant0207 <dalalnishant0207@gmail.com>
2024-04-18 20:55:00 +0530

    managing the sate of user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-// App.js
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoutes from "./Components/ProtectedRoutes.jsx";
 import Login from "./Components/LoginPage.jsx";
 import Home from "./Components/Home.jsx";
 import { FirebaseContextProvider } from "./Context/FirebaseContext.jsx";
-import { ToastContainer } from "react-toastify"; // Import ToastContainer
-import "react-toastify/dist/ReactToastify.css"; // Import Toastify CSS
+import { auth } from "./firebase"; // Import Firebase auth module
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  // While waiting for auth state to be loaded, show loading indicator
+  if (loading) {
+    return (<div>Loading...</div>);
+  }
 
   return (
     <div className="App">
@@ -17,13 +34,20 @@ function App() {
         <Router>
           <FirebaseContextProvider>
             <Routes>
-              <Route path="/" element={<Login />} />
+              <Route
+                path="/"
+                element={user ? <Navigate to="/home" /> : <Login />}
+              />
               <Route
                 path="/home"
                 element={
-                  <ProtectedRoutes>
-                    <Home />
-                  </ProtectedRoutes>
+                  user ? (
+                    <ProtectedRoutes>
+                      <Home />
+                    </ProtectedRoutes>
+                  ) : (
+                    <Navigate to="/" />
+                  )
                 }
               />
             </Routes>
